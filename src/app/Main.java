7d7573bad16fc882c71316a3d9bd3b91c1c2004d
tinoxy2k <Tinoxy2k@gmail.com>
2022-11-11 12:09:04 +0100

    Change variable position

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,13 @@
 package app;
 
 public class Main {
-
+    static int a;
+    static int b;
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
+        a = 8;
+        b = 2;
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
